464d5e959135914c9d9d5e79f80513e6e7b61a5d
Benício Januário <pbjobenicii@gmail.com>
2019-10-19 19:28:37 -0300

    Logica e construção do primeiro capítulo finalizadas!

--- GameProject/Scenes/PlayerPlaceholderScene.gd
+++ GameProject/Scenes/PlayerPlaceholderScene.gd
@@ -1,18 +1,21 @@
 extends KinematicBody2D
 
 var motion = Vector2()
-var collision
-const SPEED = 300
+const SPEED = 1000
+var elementsQuizProgress = 0
 
 func _ready():
-	get_parent().get_node('Canvas/info').visible = false
+	get_parent().get_node("Canvas/info").visible = false
 	get_parent().get_node("Canvas/Instruction01").visible = false
 	get_parent().get_node("Canvas/Instruction02").visible = false
+	closeSoulsDlg()
 	get_parent().get_node("toInstruction01").connect("timeout", self, "showInstruction01")
 	get_parent().get_node("Canvas/Instruction01/fadeAway").connect("timeout", self, "fadeInstruction01Away")
+	get_parent().get_node("dlgEnd").connect("timeout", self, "closeSoulsDlg")
+	get_parent().get_node("Canvas/mistakeMsgEnd").connect("timeout", self, "fadeOutMistakeMsg")
 	
 func _physics_process(delta):
-	collision = null
+	var collision
 	motion.x = 0
 	motion.y = 0
 	
@@ -30,13 +33,84 @@ func _physics_process(delta):
 			motion.x = -SPEED
 			
 	collision = move_and_collide(motion*delta)
+			
+	if collision:
+		if collision.collider.name == 'Soul1' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul1/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+		elif collision.collider.name == 'Soul2' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul2/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+		elif collision.collider.name == 'Soul3' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul3/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+		elif collision.collider.name == 'Soul4' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul4/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+		elif collision.collider.name == 'Soul5' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul5/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+		elif collision.collider.name == 'Soul6' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul6/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+		elif collision.collider.name == 'Soul7' and Input.is_action_pressed("ui_interact"):
+			get_parent().get_node("Souls/Soul7/dlg").visible = true
+			get_parent().get_node("dlgEnd").start()
+			
+		if collision.collider.name == "Fogo" and Input.is_action_just_released("ui_interact"):
+			if elementsQuizProgress == 0:
+				elementsQuizProgress += 1
+			else:
+				elementsQuizProgress = 0
+				get_parent().get_node("Canvas/Mistake").visible = true
+				get_parent().get_node("Canvas/mistakeMsgEnd").start()
+		
+		if collision.collider.name == "Agua" and Input.is_action_just_released("ui_interact"):
+			if elementsQuizProgress == 1:
+				elementsQuizProgress += 1
+			else:
+				elementsQuizProgress = 0
+				get_parent().get_node("Canvas/Mistake").visible = true
+				get_parent().get_node("Canvas/mistakeMsgEnd").start()
+				
+		if collision.collider.name == "Terra" and Input.is_action_just_released("ui_interact"):
+			if elementsQuizProgress == 2:
+				elementsQuizProgress += 1
+			else:
+				elementsQuizProgress = 0
+				get_parent().get_node("Canvas/Mistake").visible = true
+				get_parent().get_node("Canvas/mistakeMsgEnd").start()
+				
+		if collision.collider.name == "Ar" and Input.is_action_just_released("ui_interact"):
+			if elementsQuizProgress == 3:
+				elementsQuizProgress += 1
+			else:
+				elementsQuizProgress = 0
+				get_parent().get_node("Canvas/Mistake").visible = true
+				get_parent().get_node("Canvas/mistakeMsgEnd").start()
+			
+	if elementsQuizProgress == 4:
+		var finalDoor = get_parent().get_node("FinalDoor")
+		finalDoor.visible = false
+		finalDoor.get_node("CollisionShape2D").disabled = true
+		
+	if Input.is_action_pressed("ui_accept"):
+		closeSoulsDlg()
+			
 
-	if Input.is_action_just_released("ui_page_up") and get_parent().get_node('Canvas/info').visible == true:
+	if Input.is_action_just_released("ui_info") and get_parent().get_node('Canvas/info').visible == true:
 		get_parent().get_node('Canvas/info').visible = false
 		global.pause = false
-	elif Input.is_action_just_released("ui_page_up"):
+	elif Input.is_action_just_released("ui_info"):
 		get_parent().get_node('Canvas/info').visible = true
 		global.pause = true
+		
+func fadeOutMistakeMsg():
+	get_parent().get_node("Canvas/Mistake").visible = false
+		
+func closeSoulsDlg():
+	for soul in get_parent().get_node("Souls").get_children():
+		soul.get_node("dlg").visible = false
 		
 func showInstruction01():
 	get_parent().get_node("Canvas/Instruction01").visible = true
@@ -44,3 +118,4 @@ func showInstruction01():
 	
 func fadeInstruction01Away():
 	get_parent().get_node("Canvas/Instruction01").visible = false
+	
--- GameProject/global.gd
+++ GameProject/global.gd
@@ -2,7 +2,3 @@ extends Node
 
 # Flow Control Variables
 var pause = false
-
-# Inventory Control Arrays
-var items = [['Vinho', 'res://Assets/itemsVectors/vinho.png'], ['Enxofre', 'res://Assets/itemsVectors/enxofre.png'], ['Mercurio', 'res://Assets/itemsVectors/mercurio.png'], ['Sal', 'res://Assets/itemsVectors/sal.png'], ['Cobre', 'res://Assets/itemsVectors/cobre.png'], ['Ferro', 'res://Assets/itemsVectors/ferro.png'], ['Estanho', 'res://Assets/itemsVectors/estanho.png'], ['Chumbo', 'res://Assets/itemsVectors/chumbo.png'], ['Prata', 'res://Assets/itemsVectors/prata.png'], ['Ouro', 'res://Assets/itemsVectors/ouro.png'], ['Polvora', 'res://Assets/itemsVectors/polvora.png'], ['Acido', 'res://Assets/itemsVectors/acido.png'], ['Alcool', 'res://Assets/itemsVectors/alcool.png']]
-var inHands = [0, 0]
