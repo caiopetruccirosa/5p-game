0926112baf4d8d61c0a17e0054347f2e8e1fcd3e
caiopetruccirosa <caiopetruccirosa@gmail.com>
2019-10-20 15:09:02 -0300

    Character movimentation done.

---
+++ GameProject/scenes/ItemHandler.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name ItemHandler
+
+const ITEMS = {
+	"COLAR_BRONZE": { "name": "COLAR_BRONZE", "icon": 0 },
+	"COLAR_PRATA": { "name": "COLAR_PRATA", "icon": 1 },
+	"COLAR_OURO": { "name": "COLAR_OURO", "icon": 2 },
+	"ANEL": { "name": "ANEL", "icon": 3 },
+	"UVA": { "name": "UVA", "icon": 4 },
+	"ALCOOL": { "name": "ALCOOL", "icon": 5 },
+	"VINHO": { "name": "VINHO", "icon": 6 },
+	"FRASCO": { "name": "FRASCO", "icon": 7 },
+	"FRASCO_VINAGRE": { "name": "FRASCO_VINAGRE", "icon": 8 },
+	"FRASCO_AGUA_SAL": { "name": "FRASCO_AGUA_SAL", "icon": 9 }
+}
